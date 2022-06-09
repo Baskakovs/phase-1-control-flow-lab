33a30c1def937af869f33cbed0c0cb46b9038834
Pjotrs Baskakovs <baskakovs@me.com>
2022-06-09 17:06:55 +0300

    commit tree 96ca75301e62f5eb45ad93e11aed6343d72343e9

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  switch (true) {
+    case(feet <= 400):
+      return("This one is on me!")
+      break
+    case(feet > 400 && feet <= 2000):
+      return("That will be twenty bucks.")
+      break
+    case (feet > 2000 && feet < 2500):
+      return("I will gladly take your thirty bucks.")
+      break
+    case (feet > 2500):
+      return("No can do.")
+      break
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city==="NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (true){
+    case(tip === "generous"):
+    return "Thank you so much.";
+    break
+    case(tip === "not as generous"):
+    return "Thank you.";
+    break
+    default:
+      return "Bye."
+      break
+  }
 }
